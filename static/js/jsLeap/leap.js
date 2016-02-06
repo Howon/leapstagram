54e5ea20145ab9b6ec40e4d759b0324bca9b29e2
Mitchell Gouzenko <mgouzenko@gmail.com>
2016-02-05 21:45:54 -0500

    added leap detection

--- static/js/jsLeap/leap.js
+++ static/js/jsLeap/leap.js
@@ -1,11 +1,108 @@
 window.TO_DEG = 180 / Math.PI;
 
+const minValue = 1;
+
+function GestureState(){
+	this.numFists = 0;
+	this.openHand = 0;
+	this.isFisting = false;
+	this.locked = true;
+}
+
+const MAXXPOS = 225;
+const MAXYPOS = 185;
+const MAXZPOS = 300;
+
+function Gesture(xpos, ypos, zpos, isLocked){
+	if(!isLocked){
+		xpos -= 30;
+		ypos += 20;
+		if(xpos > MAXXPOS) xpos = MAXXPOS;
+		if(ypos > MAXYPOS) ypos = MAXYPOS;
+		if(zpos > MAXZPOS) zpos = MAXZPOS;
+		if(xpos < -MAXXPOS) xpos = -MAXXPOS;
+		if(ypos < -MAXYPOS) ypos = -MAXYPOS;
+		if(zpos < -MAXZPOS) zpos = -MAXZPOS;
+
+
+		this.xpos = xpos/MAXXPOS;
+		this.ypos = ypos/MAXYPOS;
+		this.zpos = zpos/MAXZPOS;
+	} else {
+		this.xpos = 0;
+		this.ypos = 0;
+		this.zpos = 0;
+	}
+}
+
+var gstate = new GestureState();
+
 // Set up the controller:
-Leap.loop({background: true}, {
-  hand: function(hand){
-  }
+Leap.loop({background: true}, function(frame){
+	if(frame.hands.length > 0)
+    {
+        var hand = frame.hands[0];
+        var position = hand.palmPosition;
+        var velocity = hand.palmVelocity;
+        var direction = hand.direction;
+		var locked = false;
+		//console.log(position);
+		if(checkFist(hand)){
+			if(gstate.numFists++ > 15){
+				if(!gstate.isFisting){
+					gstate.isFisting = true;
+					gstate.locked = !gstate.locked;
+				}
+			}
+		}
+		else
+		{
+			gstate.numFists = 0;
+			gstate.isFisting = false;
+		}
+
+		var gesture = new Gesture(position[0], -position[2], position[1], gstate.locked);
+		console.log(gesture);
+
+
+    }
+	else
+	{
+		gstate = new GestureState();
+	}
 });
 
+
+function getExtendedFingers(hand){
+   var f = 0;
+   for(var i=0;i<hand.fingers.length;i++){
+      if(hand.fingers[i].extended){
+         f++;
+      }
+   }
+   return f;
+}
+
+function checkFist(hand){
+   var sum = 0;
+   for(var i=0;i<hand.fingers.length;i++){
+      var finger = hand.fingers[i];
+      var meta = finger.bones[0].direction();
+      var proxi = finger.bones[1].direction();
+      var inter = finger.bones[2].direction();
+      var dMetaProxi = Leap.vec3.dot(meta,proxi);
+      var dProxiInter = Leap.vec3.dot(proxi,inter);
+      sum += dMetaProxi;
+      sum += dProxiInter
+   }
+   sum = sum/10;
+
+   if(sum<=minValue && getExtendedFingers(hand)==0){
+       return true;
+   }else{
+       return false;
+   }
+}
 
 /*********************************************************
 * The rest of the code is here for visualizing the example. Feel
