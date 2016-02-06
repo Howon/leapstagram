238ca89c7ea001289bace74e4f250a94448008c9
Kevin Lin <kl2806@columbia.edu>
2016-02-05 21:15:31 -0500

    Logic for controls

--- static/js/jsLeap/map.js
+++ static/js/jsLeap/map.js
@@ -12,12 +12,78 @@ function getLocation() {
 
 function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
+        "<br>Longitude: " + position.coords.longitude;
 }
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 8
+    });
+
 }
+
+var isLocked = false;
+var instagramLoaded = false;
+document.onkeypress = function(e) {
+    e = e || window.event;
+    var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+    // if (charCode) {
+    //     alert("Character typed: " + String.fromCharCode(charCode));
+    // }
+    key = String.fromCharCode(charCode)
+    console.log(key)
+
+    /* Navigation*/
+    if (key === 'w') {
+        /* north*/
+        if (isLocked && instagramLoaded) {
+            console.log('Image up')
+        } else if (!isLocked) {
+            map.panBy(0, -10);
+        }
+
+    } else if (key === 'd') {
+        /* east*/
+        if (isLocked && instagramLoaded) {
+            console.log('Image right')
+        } else if (!isLocked) {
+            map.panBy(10, 0);
+        }
+    } else if (key === 's') {
+        /* south*/
+        if (isLocked && instagramLoaded) {
+            console.log('Image down')
+        } else if (!isLocked) {
+            map.panBy(0, 10);
+        }
+
+    } else if (key === 'a') {
+        /* west*/
+        if (isLocked && instagramLoaded) {
+            console.log('Image west')
+        } else if (!isLocked) {
+            map.panBy(-10, 0);
+        }
+
+    } else if (key === 'j' && !isLocked) {
+        /* zoom in*/
+        map.setZoom(map.getZoom() + 1)
+
+    } else if (key === 'k' && !isLocked) {
+        /* zoom out*/
+        map.setZoom(map.getZoom() - 1)
+
+    } else if (key === 'l') { /* toggle gesture*/
+        /* isLocked*/
+        isLocked = !isLocked;
+    } else if (key === 'u' && isLocked) {
+        console.log('Instagram call')
+        instagramLoaded = !instagramLoaded;
+    } else if (key === 'i' && isLocked && instagramLoaded) {
+        console.log('tap')
+    }
+};
