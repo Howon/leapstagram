eb8540a71b70bb7ea55d3e2f43f2ee4a8e827616
Howon <howonbyun@gmail.com>
2016-02-05 21:44:34 -0500

    search box

---
+++ static/js/jsLeap/instagram.js
@@ -0,0 +1,2 @@
+clientID = "1cbe624119db410a9ee7fb0f571d5fcf";
+clientSecrete = "4e00e9e221ab4feebaffc868f1e095e8"
--- static/js/jsLeap/map.js
+++ static/js/jsLeap/map.js
@@ -1,23 +1,80 @@
-var map;
-
-var x = document.getElementById("demo");
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-}
+var map,
+  columbiaLat = 40.8093675,
+  columbiaLon = -73.9613624,
+  currentLat = columbiaLat,
+  currentLon = columbiaLon;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: columbiaLat, lng: columbiaLon},
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
   });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+  detectMovement(map);
 }
+
+var detectMovement = function(map){
+  console.log(map.getCenter());
+}
+
+var getImagesOnCurrentLocation = function(lat, lon){
+  console.log(lat + " " + lon);
+}
+
+
